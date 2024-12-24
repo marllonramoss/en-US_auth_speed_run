c8e8d81a42bbede00eda66477f919701e26a0d75
Marllon Ramos <marllonsteam@gmail.com>
2024-12-24 10:11:19 -0300

    starting basic components

---
+++ apps/frontend/src/app/(paginas)/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "@/components/auth/LoginForm";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default page;
---
+++ apps/frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <span>Main App</span>
+    </div>
+  );
+};
+
+export default page;
